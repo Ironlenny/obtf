31e3f2500d2b3b88e653462eebeb34fcbb44e902
Jacob Riddle <ironlenny@gmail.com>
2023-01-18 20:53:21 -0800

    Event handler implemented

--- OBTF.lua
+++ OBTF.lua
@@ -1,8 +1,8 @@
-Obtf = {}
-
+Obtf = CreateFrame("Frame")
 Obtf.blacklist = {"Dalaran"}
+Obtf:RegisterEvent("ZONE_CHANGED")
 
-function Obtf:CheckZone()
+function Obtf:CheckZone(event, ...)
   local zone = GetSubZoneText()
   for i= 0, 1 do
     if zone == self.blacklist[i] then
@@ -14,7 +14,11 @@ function Obtf:CheckZone()
 end
 
 function Obtf:UpdateFlyingMacro()
+  print("Can fly")
 end
 
 function Obtf:UpdateNoFlyMacro()
+  print("Cannot fly")
 end
+
+Obtf:SetScript("OnEvent", Obtf.CheckZone)
