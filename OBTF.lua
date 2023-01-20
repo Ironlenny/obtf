af4791edeed41285c9efeb3a52ab0f09351d42d7
Jacob Riddle <ironlenny@gmail.com>
2023-01-19 19:22:08 -0800

    Macro updates correctly.

--- OBTF.lua
+++ OBTF.lua
@@ -1,11 +1,24 @@
 Obtf = CreateFrame("Frame")
 Obtf.blacklist = {"Dalaran"}
+Obtf.body = "#showtooltop\n/cast [swimming] Aquatic Form; "
+Obtf.outdoors_nofly = "[outdoors, noflyable]"
+Obtf.outdoors = "[outdoors]"
+Obtf.travelForm = "Travel Form"
+Obtf.combat = "[combat]"
+Obtf.swiftFlightForm = "Swift Flight Form"
+Obtf.flightForm = "Flight Form"
+Obtf.flyable = "[flyable, nocombat]"
 Obtf:RegisterEvent("ZONE_CHANGED")
 
-function Obtf:CheckZone(event, ...)
+function Obtf:OnEvent()
+  local delay = 2
+  C_Timer.After(delay, Obtf.CheckZone)
+end
+
+function Obtf:CheckZone()
   local zone = GetSubZoneText()
-  for i= 0, 1 do
-    if zone == self.blacklist[i] then
+  for i= 1, 1 do
+    if zone == Obtf.blacklist[i] then
       Obtf:UpdateNoFlyMacro()
     else
       Obtf:UpdateFlyingMacro()
@@ -14,11 +27,23 @@ function Obtf:CheckZone(event, ...)
 end
 
 function Obtf:UpdateFlyingMacro()
-  print("Can fly")
+  local flightForm = self.swiftFlightForm
+  local body = self.body .. self.flyable .. " " .. flightForm .. "; " .. self.outdoors_nofly .. " " .. self.travelForm .. "; " .. self.combat .. " " .. self.travelForm
+  self:UpdateMacro(body)
 end
 
 function Obtf:UpdateNoFlyMacro()
-  print("Cannot fly")
+  local body = self.body .. self.outdoors .. " " .. self.travelForm .. "; " .. self.combat .. " " .. self.travelForm
+  self:UpdateMacro(body)
 end
 
-Obtf:SetScript("OnEvent", Obtf.CheckZone)
+function Obtf:UpdateMacro(body)
+  macroIndex = GetMacroIndexByName("OBTF")
+  if macroIndex == 0 then
+    CreateMacro("OBTF", "INV_MISC_QUESTIONMARK", body, nil)
+  else
+    EditMacro("OBTF", "OBTF", nil, body, 1, 1)
+  end
+end
+
+Obtf:SetScript("OnEvent", Obtf.OnEvent)
