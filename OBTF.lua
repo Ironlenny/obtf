0d6d8b4855019f850b5098b37ad131d0690ee775
Jacob Riddle <ironlenny@gmail.com>
2023-01-19 22:24:53 -0800

    Release version

--- OBTF.lua
+++ OBTF.lua
@@ -1,20 +1,61 @@
 Obtf = CreateFrame("Frame")
 Obtf.blacklist = {"Dalaran"}
-Obtf.body = "#showtooltop\n/cast [swimming] Aquatic Form; "
 Obtf.outdoors_nofly = "[outdoors, noflyable]"
 Obtf.outdoors = "[outdoors]"
+Obtf.swimming = "[swimming]"
+Obtf.aquaticForm = "Aquatic Form"
 Obtf.travelForm = "Travel Form"
 Obtf.combat = "[combat]"
 Obtf.swiftFlightForm = "Swift Flight Form"
 Obtf.flightForm = "Flight Form"
 Obtf.flyable = "[flyable, nocombat]"
 Obtf:RegisterEvent("ZONE_CHANGED")
+Obtf:RegisterEvent("PLAYER_ENTERING_WORLD")
+Obtf.forms = {}
+Obtf.forms[Obtf.swiftFlightForm] = 40120
+Obtf.forms[Obtf.flightForm] = 33943
+Obtf.forms[Obtf.travelForm] = 783
+Obtf.forms[Obtf.aquaticForm] = 1066
+Obtf.noFlyMacro = ""
+Obtf.flyMacro = ""
 
-function Obtf:OnEvent()
-  local delay = 1
-  C_Timer.After(delay, Obtf.CheckZone)
+-- Initialize the macro strings. Check which forms are available
+function Obtf:Init()
+  local start = "#showtooltop\n/cast "
+  local noFlyMacro = start
+  local flyMacro = start
+
+  if IsSpellKnown(self.forms[self.aquaticForm]) then
+    noFlyMacro = noFlyMacro .. self.swimming .. " " .. self.aquaticForm .. "; "
+    flyMacro = flyMacro .. self.swimming .. " " .. self.aquaticForm .. "; "
+  end
+
+  if IsSpellKnown(self.forms[self.travelForm]) then
+    noFlyMacro = noFlyMacro .. self.combat .. " " .. self.travelForm .. "; " .. self.outdoors .. " " .. self.travelForm
+    flyMacro = flyMacro .. self.combat .. " " .. self.travelForm .. "; " .. self.outdoors_nofly .. " " .. self.travelForm .. "; "
+  end
+
+  if IsSpellKnown(self.forms[self.flightForm]) then
+    flyMacro = flyMacro .. self.flyable .. " " .. self.flightForm
+  elseif IsSpellKnown(self.forms[self.swiftFlightForm]) then
+    flyMacro = flyMacro .. self.flyable .. " " .. self.swiftFlightForm
+  end
+
+  self.noFlyMacro = noFlyMacro
+  self.flyMacro = flyMacro
 end
 
+-- Event handler
+function Obtf:OnEvent(isLogin, isReload)
+  local delay = 1
+  if isLogin or isReload then
+    Obtf:Init() -- Must use Obtf instead of self, as self is not passed into callbacks.
+  else
+    C_Timer.After(delay, Obtf.CheckZone)
+  end
+end
+
+-- Check what's the player's subzone and call the approriate macro update function.
 function Obtf:CheckZone()
   local zone = GetSubZoneText()
   for i= 1, 1 do
@@ -27,16 +68,14 @@ function Obtf:CheckZone()
 end
 
 function Obtf:UpdateFlyingMacro()
-  local flightForm = self.swiftFlightForm
-  local body = self.body .. self.flyable .. " " .. flightForm .. "; " .. self.outdoors_nofly .. " " .. self.travelForm .. "; " .. self.combat .. " " .. self.travelForm
-  self:UpdateMacro(body)
+  self:UpdateMacro(self.flyMacro)
 end
 
 function Obtf:UpdateNoFlyMacro()
-  local body = self.body .. self.outdoors .. " " .. self.travelForm .. "; " .. self.combat .. " " .. self.travelForm
-  self:UpdateMacro(body)
+  self:UpdateMacro(self.noFlyMacro)
 end
 
+-- Updates the macro with the passed in body
 function Obtf:UpdateMacro(body)
   macroIndex = GetMacroIndexByName("OBTF")
   if macroIndex == 0 then
